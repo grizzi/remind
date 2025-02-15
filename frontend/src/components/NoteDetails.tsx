25fc71b4abbe5978768d49b2cdfd2d2301a3c007
Giuseppe Rizzi <giuseppe.rizzi10@gmail.com>
2025-02-15 11:33:52 +0100

    adding tailwind css

--- frontend/src/components/NoteDetails.tsx
+++ frontend/src/components/NoteDetails.tsx
@@ -9,11 +9,14 @@ const NoteDetails = ({
 }) => {
   const formattedDate = new Date(note.created_at).toLocaleDateString('en-US')
   return (
-    <div className='note-container'>
-      <p className='note-title'>{note.title}</p>
-      <p className='note-contant'>{note.content}</p>
+    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <p className='font-bold text-xl mb-2'>{note.title}</p>
+      <p className='text-gray-700 text-base'>{note.content}</p>
       <p className='note-date'>{formattedDate}</p>
-      <button className='delete-button' onClick={() => onDelete(note.id)}>
+      <button
+        className='rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'
+        onClick={() => onDelete(note.id)}
+      >
         Delete
       </button>
     </div>
