836771f3cd10904c2e12b1b18c4c2da4832a0243
Giuseppe Rizzi <giuseppe.rizzi10@gmail.com>
2025-03-29 14:23:06 +0100

    handling different screen sizes

--- frontend/src/pages/SubscriptionsPage.tsx
+++ frontend/src/pages/SubscriptionsPage.tsx
@@ -21,11 +21,11 @@ const MetricCard = ({
   return (
     <div className='flex flex-row bg-gradient-to-r from-pink-500 to-violet-500 text-white rounded-xl p-1 shadow-md w-full m-2'>
       <div className='w-full flex flex-col'>
-        <p className='pl-2 pt-2 text-3xl'>{header}</p>
+        <p className='pl-2 pt-2 text-md sm:text-3xl '>{header}</p>
 
         <div className='flex flex-row justify-end min-h-[2.5rem] p-1 mr-2'>
           {!loadingDetails ? (
-            <p className='text-xl'>{details}</p>
+            <p className='text-sm sm:text-xl'>{details}</p>
           ) : (
             <div className='h-6 w-32 animate-pulse opacity-0 rounded-md bg-white/50 backdrop-blur-2xl shadow-inner' />
           )}
