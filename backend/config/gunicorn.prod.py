919fc1880e4bcfe9886f18d8331c547b2a44c623
Ubuntu <ubuntu@ip-172-31-30-5.eu-central-1.compute.internal>
2025-05-19 19:10:05 +0000

    experimenting on ec2

--- backend/config/gunicorn.prod.py
+++ backend/config/gunicorn.prod.py
@@ -10,14 +10,14 @@ workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:8000"
 
 # Write access and error info to /var/log
-accesslog = "/var/log/gunicorn/access.log"
-errorlog = "/var/log/gunicorn/error.log"
+accesslog = "/etc/backend/gunicorn.access.log"
+errorlog = "/etc/backend/gunicorn.error.log"
 
 # Redirect stdout/stderr to log file
 capture_output = True
 
 # PID file so you can easily fetch process ID
-pidfile = "/var/run/gunicorn/prod.pid"
+pidfile = "/etc/backend/gunicorn.prod.pid"
 
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = False
